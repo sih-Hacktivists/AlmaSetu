373ec71c6d7680a1cf8e3fb06290feb598a60ab9
AKSAR <akshatjaiswal2518@gmail.com>
2024-09-01 13:20:07 +0530

    add utilities and middlewares

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,59 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { Admin } from "../models/admin.model.js";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+    try {
+        const token =
+            req.cookies?.accessToken ||
+            req.header("Authorization")?.replace("Bearer ", "");
+
+        if (!token) {
+            throw new ApiError(401, "Unauthorized request");
+        }
+
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+        const user = await User.findById(decodedToken._id).select(
+            "-password -refreshToken"
+        );
+
+        if (!user) {
+            throw new ApiError(401, "Invalid Access Token");
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new ApiError(401, "Invalid Access Token");
+    }
+});
+
+export const verifyAdmin = asyncHandler(async (req, res, next) => {
+    try {
+        const token =
+            req.cookies?.accessToken ||
+            req.header("Authorization")?.replace("Bearer ", "");
+
+        if (!token) {
+            throw new ApiError(401, "Unauthorized request");
+        }
+
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+        const admin = await Admin.findById(decodedToken._id).select(
+            "-password -refreshToken"
+        );
+
+        if (!admin) {
+            throw new ApiError(401, "Invalid Access Token");
+        }
+
+        req.admin = admin;
+        next();
+    } catch (error) {
+        throw new ApiError(401, "Invalid Access Token");
+    }
+});
