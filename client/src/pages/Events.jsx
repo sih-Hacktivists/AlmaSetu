cf042e97deda63e413e765b2990b4d660dc94a3c
PrithviS18 <prithvisen003@gmail.com>
2024-09-09 14:17:35 +0530

    Changed images

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -14,15 +14,15 @@ export default function Events() {
             <div className='flex w-full h-20 items-center m-6'>
                 {/* <img className="w-20 h-10 basis-1/12" src={Bar} /> */}
                 <div className="flex items-center justify-center basis-11/12">
-                    <div className="w-3/5"><SearchBar showProfile={false} superAdmin={false} /></div>
+                    <div className="w-3/5"><SearchBar showProfile={false} superAdmin={false} showSearch={true}/></div>
                 </div>
             </div>
 
             {/* Your Events */}
-            <div className="flex w-[90vw] flex-col items-start justify-center mb-8">
+            <div className="flex w-[90vw] flex-col items-start justify-center mb-5">
                 <div className="flex gap-10">
                     <p className='bg-[#0F1035] mb-6 text-white w-48 h-10 text-2xl rounded-[25px] flex items-center justify-center gap-2'>Your Events</p>
-                    {alumni ? <button className='bg-[#0F1035] mb-6 text-white w-48 h-10 hover:bg-[#497fa1] hover:text-black text-2xl rounded-[25px] flex items-center justify-center gap-2'>Create Event <b className='text-2xl'>+</b></button> : null}
+                    {alumni ? <button className='bg-[#0F1035] mb-3 text-white w-48 h-10 hover:bg-[#497fa1] hover:text-black text-2xl rounded-[25px] flex items-center justify-center gap-2'>Create Event <b className='text-2xl'>+</b></button> : null}
                 </div>
                 <div className="overflow-x-auto">
                     <YourEvents />
@@ -34,7 +34,7 @@ export default function Events() {
                 <div className="bg-[#0F1035] rounded-t-md text-white flex items-center justify-center text-xl h-14">
                     Recommended Events For You
                 </div>
-                <div className="row-span-3 p-2 scrollbar-custom overflow-x-auto m-5">
+                <div className="flex flex-col gap-20 row-span-3 p-2 scrollbar-custom overflow-x-auto">
                     <TopEvents />
                 </div>
             </div>
