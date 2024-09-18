596ac5911e684f45f51189d10eca1fea9b407013
Rahul Das <121535378+RAHULDAS6009@users.noreply.github.com>
2024-09-19 02:47:59 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      "/api/v1": "https://almasetu-ye1z.onrender.com",
+      '/api/v1': {
+        target: 'https://almasetu-ye1z.onrender.com',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api\/v1/, '')
+      },
     },
   },
   plugins: [react()],
