2191af8ce9d7b1994c8d3c874c1cdb10a5d6cdf1
Rahul Das <das206053@gmail.com>
2024-09-19 08:39:36 +0530

    Chat half complete

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api/v1': {
-        target: 'https://almasetu-ye1z.onrender.com',
-        changeOrigin:true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api\/v1/, '')
-      },
+      "/api/v1": "http://localhost:3000  ",
     },
   },
   plugins: [react()],
