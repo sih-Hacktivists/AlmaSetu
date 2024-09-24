0207129b9568b9f420ec9759f336e6ab885b1e29
Rahul Das <das206053@gmail.com>
2024-09-25 03:07:14 +0530

    deployment fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    historyApiFallback: true, // Ensure fallback is set
     proxy: {
-      "/api/v1": "http://localhost:3000",
+      "/api/v1": "https://almasetu.onrender.com",
+      changeOrigin: true,
+      secure: false,
     },
   },
   plugins: [react()],
